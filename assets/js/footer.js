f7b7905feac71720e0ad92268bc018388988a54c
Esherloon <jinoo2005609@gmail.com>
2023-05-06 13:23:40 +0900

    Footer change

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,1 +1,1 @@
-document.getElementById('footer').innerHTML = 'Esherloon''s Server (에셜룬의 서버) | <a href="https://esherloon.com/">esherloon.com</a>';
+document.getElementById('footer').innerHTML = "Esherloon's Server (에셜룬의 서버) | <a href="https://esherloon.com/">esherloon.com</a>";
